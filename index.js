5cfe222079cf6750d584533a16037e30ce253a19
Mike Morton <mmorton7295@gmail.com>
2021-07-14 21:01:32 -0500

    adds a new console.log to test the webpack.config.js file

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ var moment = require('moment');
 var math = require('mathjs');
 console.log(moment().startOf('day').fromNow());
 console.log(math.sqrt(4).toString());
+console.log("hello there new stuff was added");
